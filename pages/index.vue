cd543996c553a5854acfae09eaad5ec455b9e6ac
lebLuiz <leb.e.b@hotmail.com>
2023-12-12 22:52:59 -0300

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const {x, y} = useMouse();
+const color = useColorMode();
+
+function toggleDark() {
+    color.value = color.value === 'dark' ? 'light' : 'dark';
+}
+
+</script>
+
+<template>
+    <div class="text-red">
+        <span>Hello World {{ x }}, {{ y }}</span>
+
+        <br>
+
+        <button @click="toggleDark" v-text="color.value" />
+    </div>
+</template>
